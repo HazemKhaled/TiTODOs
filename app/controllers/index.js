b6b8a89b1128745bb86a4d045497d0a94b96571c
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-11 20:02:49 +0200

    Now ListView loaded from database directly, we have a prettyDate, thanks for MomentJS, also todos sorted by lastModifiedDate DESC

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,6 +3,15 @@ function productListClicked(e) {
 }
 
 // Load the data into the collection, collection will bind the data into the ListView automaticly
-Alloy.Collections.tasks.fetch();
+Alloy.Collections.tasks.fetch({
+  query: "SELECT * FROM tasks ORDER BY lastModifiedDate DESC"
+});
+
+function transfomer(model) {
+  var transform = model.toJSON();
+
+  transform.prettyTime = Alloy.Globals.moment(transform.lastModifiedDate).fromNow();
+  return transform;
+}
 
 Alloy.Globals.pageStack.open($.index);
