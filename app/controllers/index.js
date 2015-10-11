f881da39c427b6f143ebfe41719170ef23318dd0
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-11 19:52:49 +0200

    Create the data model, then load it into the ListView

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,7 @@ function productListClicked(e) {
 
 }
 
+// Load the data into the collection, collection will bind the data into the ListView automaticly
+Alloy.Collections.tasks.fetch();
+
 Alloy.Globals.pageStack.open($.index);
