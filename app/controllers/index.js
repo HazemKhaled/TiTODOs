6e127d7ad97b2fa7e4acd5c62581d90888604da5
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-11 20:26:32 +0200

    implement the mark as completed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,37 @@
-function productListClicked(e) {
+function todoListClicked(e) {
+  // get the clicked item from that section
+  var item = e.section.getItemAt(e.itemIndex);
 
+  // clicked row
+  Ti.API.debug(JSON.stringify(item));
+
+  var options = Ti.UI.createOptionDialog({
+    cancel: 2,
+    title: item.text.text,
+    options: ['Completed', 'Cancel'],
+  });
+
+  options.addEventListener('click', function(eOptions) {
+    switch (eOptions.index) {
+      case 0:
+        var myModel = Alloy.Collections.tasks.at(e.itemIndex);
+        myModel.set('status', 'completed');
+        myModel.set('lastModifiedDate', Alloy.Globals.moment().toISOString());
+        myModel.save();
+        loadtasks();
+        break;
+    }
+  });
+  options.show();
 }
 
-// Load the data into the collection, collection will bind the data into the ListView automaticly
-Alloy.Collections.tasks.fetch({
-  query: "SELECT * FROM tasks ORDER BY lastModifiedDate DESC"
-});
+function loadtasks() {
+  // Load the data into the collection, collection will bind the data into the ListView automaticly
+  Alloy.Collections.tasks.fetch({
+    query: "SELECT * FROM tasks ORDER BY lastModifiedDate DESC"
+  });
+}
+loadtasks();
 
 function transfomer(model) {
   var transform = model.toJSON();
