1feb7bd21ba7bb56a21fe355e562ad0e3fd99e31
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-12 21:09:37 +0200

    Upload image from gallery or camera on both edit or add

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -4,6 +4,7 @@ if (args.hasOwnProperty('itemIndex')) {
   // Edit mode
   var myModel = Alloy.Collections.tasks.at(args.itemIndex);
   $.text.value = myModel.get('text');
+  $.image.image = myModel.get('image');
 } else {
   // Add mode
   var myModel = Alloy.createModel('tasks');
@@ -13,6 +14,55 @@ if (args.hasOwnProperty('itemIndex')) {
 // Focus on 1st input
 function windowOpened() {
   $.text.focus();
+}
+
+function showImageOptionDialog() {
+
+  var options = Ti.UI.createOptionDialog({
+    cancel: 2,
+    title: 'From?',
+    options: ['Camera', 'Gallery', 'Cancel'],
+  });
+
+  options.addEventListener('click', function(eOptions) {
+    switch (eOptions.index) {
+      case 0:
+        Ti.Media.showCamera({
+          allowEditing: true,
+          saveToPhotoGallery: true,
+          mediaTypes: [Titanium.Media.MEDIA_TYPE_PHOTO],
+          success: function(event) {
+            alert(event.media.nativePath);
+            $.image.image = event.media;
+            myModel.set('image', event.media.nativePath);
+          },
+          error: function(errorEvent) {
+            alert("Hey, It's not working on simulator");
+          }
+        });
+        break;
+      case 1:
+        Ti.Media.openPhotoGallery({
+          allowEditing: true,
+          mediaTypes: [Titanium.Media.MEDIA_TYPE_PHOTO],
+          success: function(event) {
+            var image = event.media,
+              fileName = Math.random() + '.png';
+
+            var writeFile = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, fileName);
+            writeFile.write(image);
+
+            $.image.image = writeFile.nativePath;
+            myModel.set('image', writeFile.nativePath);
+          },
+          error: function() {
+            alert('Sorry, something happen wrong, try again.');
+          }
+        });
+        break;
+    }
+  });
+  options.show();
 }
 
 function saveBtnClicked() {
